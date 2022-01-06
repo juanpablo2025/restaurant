3aa172241b2c1bc9e0d4a4c84a977ca78a5bb4c6
Juan Avedaño <juanpablo_1810@hotmail.com>
2022-01-06 18:37:24 -0500

    MAIN

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -10,8 +10,24 @@ export default async function handler(req,res){
 	 
 	if(method==='GET'){
 		try{
-			const products = await products
+			const products = await products.find();
+			res.status(200).json(products);
+
+		}catch(err){
+			res.status(500).json(err);
 		}
 
+	}
+
+	if(method === "POST"){
+		if(!token||token !== process.env.token){
+			return res.status(401).json("Not authenticated!")
+		}try{
+			const product = await Product.create(req.body);
+			res.status(201).json(product);
+		}catch(err){
+			res.status(500).json(err);
+		}
+		
 	}
 }
